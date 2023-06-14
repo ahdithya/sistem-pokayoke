be13ae3c493d2e6a43dd8cbbe7b16e274b21b6d9
Aditya <ahdithiya@gmail.com>
2023-06-14 15:30:00 +0700

    adding new route on workOrderRoute

--- src/routes/v1/workOrderRoute.js
+++ src/routes/v1/workOrderRoute.js
@@ -2,14 +2,16 @@ const express = require("express");
 const { uploadFile } = require("../../middlewares/multer");
 const router = express.Router();
 const workOrderController = require("../../controllers/workOrderController");
-const { authJWT } = require("../../middlewares/auth");
 
-router.route("/").get().post(authJWT, workOrderController.createWorkOrder);
+router
+  .route("/")
+  .get(workOrderController.getAllWorkOrder)
+  .post(workOrderController.createWorkOrder);
 
 router
   .route("/upload-file")
   .post(uploadFile.single("file"), workOrderController.uploadOrder);
 
-router.route("/:id").get().put().delete();
+router.route("/:id").get(workOrderController.getOneWorkOrder).put().delete();
 
 module.exports = router;
